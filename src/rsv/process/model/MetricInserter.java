ee5808674240170f0e8b59b17c677b5bda0d5197
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-08-11 17:49:17 +0000

    Added logic to search for inserted detail within its own batch

--- src/rsv/process/model/MetricInserter.java
+++ src/rsv/process/model/MetricInserter.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLWarning;
 import java.sql.Statement;
+import java.util.HashMap;
 
 import org.apache.log4j.Logger;
 
@@ -17,9 +18,11 @@ public class MetricInserter extends RSVDatabase {
 	private PreparedStatement stmt_data = null;
 	private PreparedStatement stmt_detail = null;	
 	private PreparedStatement stmt_detail_check = null;
+	private HashMap<String, Integer> detail_in_batch = new HashMap<String, Integer>();
 	
 	public MetricInserter()
 	{
+		HashMap<String, Integer> detail_in_batch = new HashMap<String, Integer>();
 		try {
 			String sql = "insert into metricdata (id, timestamp, resource_id, metric_id, metric_status_id, detail_id) values (?,?,?,?,?,?)";
 		    stmt_data = RSVDatabase.db.prepareStatement(sql);
@@ -56,26 +59,35 @@ public class MetricInserter extends RSVDatabase {
 	
 	public void add(int id, int timestamp, int resource_id, int metric_id, int status_id, String detail) throws SQLException 
 	{	
-		//look up to see if the same detail already exists
-		stmt_detail_check.setString(1, detail);
-		ResultSet res = stmt_detail_check.executeQuery();
 		Integer detail_id = id;
-		if(res.next()) {
-			detail_id = res.getInt(1);
+		
+		//did we just inserted this already?
+		Integer existing_id = detail_in_batch.get(detail);
+		if(existing_id != null) {
+			detail_id = existing_id;
 		} else {
-			//doesn't exist. insert new detail
-			stmt_detail.setInt(1, id);
-			stmt_detail.setString(2, detail);
-			stmt_detail.addBatch();
+			//now look up on DB to see if the same detail already exists	
+			stmt_detail_check.setString(1, detail);
+			ResultSet res = stmt_detail_check.executeQuery();
+			if(res.next()) {
+				detail_id = res.getInt(1);
+			} else {
+				//doesn't exist. insert new detail
+				stmt_detail.setInt(1, id);//use the same key as in the gratia DB
+				stmt_detail.setString(2, detail);
+				stmt_detail.addBatch();
+				
+				detail_in_batch.put(detail, id);
+			}
 		}
 
 		//insert metricdata
-		stmt_data.setInt(1, id);
+		stmt_data.setInt(1, id); //use the same key as in the gratia DB
 		stmt_data.setInt(2, timestamp);
 		stmt_data.setInt(3, resource_id);
 		stmt_data.setInt(4, metric_id);
 		stmt_data.setInt(5, status_id);
-		stmt_data.setInt(6, id);
+		stmt_data.setInt(6, detail_id); //it could be the gratia DB (for now detail) or existing detail_id
 		stmt_data.addBatch();
 	}
 	
