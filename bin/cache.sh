55bd3cb9bfab862b2060f37a7821d1265bfebdab
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-07-22 18:02:44 +0000

    updated the JAVA_HOME to use standard location

--- bin/cache.sh
+++ bin/cache.sh
@@ -1,5 +1,5 @@
 echo "create cache"
-export JAVA_HOME=/home/hayashis/app/jdk1.6.0_07
+export JAVA_HOME=/usr/lib/jvm/java-sun
 export PATH=$JAVA_HOME/bin:$PATH
 export CLASSPATH=lib/log4j-1.2.15.jar:lib/mysql-connector-java-5.1.6-bin.jar:rsvprocess.jar
 cd /usr/local/rsvprocess
--- bin/recalc.sh
+++ bin/recalc.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-export JAVA_HOME=/usr/local/jdk160
+export JAVA_HOME=/usr/lib/jvm/java-sun
 export PATH=$JAVA_HOME/bin:$PATH
 export RSVPROCESS_HOME=/usr/local/rsvprocess
 export CLASSPATH=lib/log4j-1.2.15.jar:lib/mysql-connector-java-5.1.6-bin.jar:rsvprocess.jar
