3635e18e743bda0dd766f4f2cb5434a1ed363eb1
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-11-16 14:18:43 +0000

    version number increment

--- src/rsv/process/control/RSVMain.java
+++ src/rsv/process/control/RSVMain.java
@@ -23,7 +23,7 @@ public class RSVMain {
 	private static final Logger logger = Logger.getLogger(RSVMain.class);
 	public static Configuration conf = null;
 	
-	public static final String version = "1.9";
+	public static final String version = "1.11";
 	
 	public static boolean debug = false;
 
