90e2765d6e16bdaaf1745118eae3c3a4a867ea45
/C=US/ST=Indiana/L=Bloomington/O=Open Science Grid/OU=Grid Operations Center/CN=*.grid.iu.edu/svn/emailAddress=goc@opensciencegrid.org </C=US/ST=Indiana/L=Bloomington/O=Open Science Grid/OU=Grid Operations Center/CN=*.grid.iu.edu/svn/emailAddress=goc@opensciencegrid.org@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-12-08 14:57:26 +0000

    fixed some issue with vocache.sh

--- bin/vocache.sh
+++ bin/vocache.sh
@@ -1,8 +1,6 @@
-echo "create vomatrix cache"
-export JAVA_HOME=/usr/local/jdk160
+#!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-sun
 export PATH=$JAVA_HOME/bin:$PATH
 export CLASSPATH=lib/log4j-1.2.15.jar:lib/mysql-connector-java-5.1.6-bin.jar:rsvprocess.jar
-cd /usr/local/rsvprocess
-ant
-java rsv.process.control.RSVMain vomatrix
-cd -
+ant -f /usr/local/rsvprocess/build.xml vomatrix
+#java rsv.process.control.RSVMain vomatrix
