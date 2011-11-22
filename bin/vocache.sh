632b9f4d092267b1ca6cee12cc7691a2bb58aed9
/C=US/ST=Indiana/L=Bloomington/O=Open Science Grid/OU=Grid Operations Center/CN=*.grid.iu.edu/svn/emailAddress=goc@opensciencegrid.org </C=US/ST=Indiana/L=Bloomington/O=Open Science Grid/OU=Grid Operations Center/CN=*.grid.iu.edu/svn/emailAddress=goc@opensciencegrid.org@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2011-11-22 14:55:31 +0000

    updated the cache/vomatrix run script

--- bin/vocache.sh
+++ bin/vocache.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 export JAVA_HOME=/usr/lib/jvm/java-sun
 export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=lib/log4j-1.2.15.jar:lib/mysql-connector-java-5.1.6-bin.jar:rsvprocess.jar
-ant -f /usr/local/rsvprocess/build.xml vomatrix
-#java rsv.process.control.RSVMain vomatrix
+#export CLASSPATH=lib/log4j-1.2.15.jar:lib/mysql-connector-java-5.1.6-bin.jar:rsvprocess.jar
+#ant -f /usr/local/rsvprocess/build.xml vomatrix
+
+
+export RSVPROCESS_HOME=/usr/local/rsvprocess
+ant -f $RSVPROCESS_HOME/build.xml vomatrix
+if [ ! $? -eq 0 ]; then
+    echo "vomatrix has failed"
+fi;
+
+
