30100b81cc1fefda313a61778b0922b5ff849bb7
/C=US/ST=Indiana/L=Bloomington/O=Open Science Grid/OU=Grid Operations Center/CN=*.grid.iu.edu/svn/emailAddress=goc@opensciencegrid.org </C=US/ST=Indiana/L=Bloomington/O=Open Science Grid/OU=Grid Operations Center/CN=*.grid.iu.edu/svn/emailAddress=goc@opensciencegrid.org@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2011-11-22 14:57:55 +0000

    (patched) updated myosg/rsvprocess invocation script

--- bin/cache.sh
+++ bin/cache.sh
@@ -1,10 +1,17 @@
 echo "create cache"
 export JAVA_HOME=/usr/lib/jvm/java-sun
 export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=lib/log4j-1.2.15.jar:lib/mysql-connector-java-5.1.6-bin.jar:rsvprocess.jar
-cd /usr/local/rsvprocess
-ant
-java rsv.process.control.RSVMain cache
-cd -
+#export CLASSPATH=lib/log4j-1.2.15.jar:lib/mysql-connector-java-5.1.6-bin.jar:rsvprocess.jar
+#cd /usr/local/rsvprocess
+#ant
+#java rsv.process.control.RSVMain cache
+#cd -
+
+
+export RSVPROCESS_HOME=/usr/local/rsvprocess
+ant -f $RSVPROCESS_HOME/build.xml cache
+if [ ! $? -eq 0 ]; then
+    echo "cache has failed"
+fi;
 
 
