bc55a364945dc73ad6db32a8274f63805cc14a80
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2012-02-07 19:34:57 +0000

    1.18 begins

--- src/rsv/process/control/RSVMain.java
+++ src/rsv/process/control/RSVMain.java
@@ -22,7 +22,7 @@ public class RSVMain {
 	private static final Logger logger = Logger.getLogger(RSVMain.class);
 	public static Configuration conf = null;
 	
-	public static final String version = "1.17";
+	public static final String version = "1.18";
 	
 	public static boolean debug = false;
 
