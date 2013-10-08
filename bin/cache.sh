65bde0298277797442ffa80f101d9c549ea400c3
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=Services/CN=stemcell.grid.iu.edu </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=Services/CN=stemcell.grid.iu.edu@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2013-10-08 17:28:54 +0000

    updated to use java-openjdk

--- bin/cache.sh
+++ bin/cache.sh
@@ -1,5 +1,5 @@
 echo "create cache"
-export JAVA_HOME=/usr/lib/jvm/java-sun
+export JAVA_HOME=/usr/lib/jvm/java-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 #export CLASSPATH=lib/log4j-1.2.15.jar:lib/mysql-connector-java-5.1.6-bin.jar:rsvprocess.jar
 #cd /usr/local/rsvprocess
